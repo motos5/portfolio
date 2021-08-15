3fe7bd340dd4d479509ef9a54387897bb76d9072
motos5 <allmarket198@gmail.com>
2021-08-15 10:53:20 +0300

    added header skills and about

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,54 @@
-$(function () {});
+$(function () {
+
+    var myCircle = Circles.create({
+        id:                  'circles-1',
+        radius:              80,
+        value:               85,
+        maxValue:            100,
+        width:               10,
+        text:                function(value){return value + '%';},
+        colors:              ['#89D5FA', '#0889C9'],
+        duration:            400,
+        wrpClass:            'circles-wrp',
+        textClass:           'circles-text',
+        valueStrokeClass:    'circles-valueStroke',
+        maxValueStrokeClass: 'circles-maxValueStroke',
+        styleWrapper:        true,
+        styleText:           true
+      });
+
+    var myCircle = Circles.create({
+        id:                  'circles-2',
+        radius:              80,
+        value:               20,
+        maxValue:            100,
+        width:               10,
+        text:                function(value){return value + '%';},
+        colors:              ['#FCEBA2', '#F6CB0E'],
+        duration:            400,
+        wrpClass:            'circles-wrp',
+        textClass:           'circles-text',
+        valueStrokeClass:    'circles-valueStroke',
+        maxValueStrokeClass: 'circles-maxValueStroke',
+        styleWrapper:        true,
+        styleText:           true
+      });
+
+    var myCircle = Circles.create({
+        id:                  'circles-3',
+        radius:              80,
+        value:               55,
+        maxValue:            100,
+        width:               10,
+        text:                function(value){return value + '%';},
+        colors:              ['#FCBFB0', '#F6491E'],
+        duration:            400,
+        wrpClass:            'circles-wrp',
+        textClass:           'circles-text',
+        valueStrokeClass:    'circles-valueStroke',
+        maxValueStrokeClass: 'circles-maxValueStroke',
+        styleWrapper:        true,
+        styleText:           true
+      });
+
+});
