9314c0035b3d27bc88c32dd389bfbc6e2ef77b48
Виталий Изотов <allmarket198@gmail.com>
2023-03-10 19:47:58 +0200

    Added footer for Marcho

--- wp-content/themes/marcho/footer.php
+++ wp-content/themes/marcho/footer.php
@@ -1,3 +1,120 @@
+<footer class="footer">
+    <div class="container">
+        <div class="footer-top">
+            <div class="footer-top__item footer-top__item-contact">
+                <a class="logo footer-top__logo" href="#">
+                    <img class="logo__img" src="<?php echo get_template_directory_uri() ?>/app/images/logo.svg" alt="Logo">
+                </a>
+                <a class="footer-top__address" href="#">
+                    No. 342 - London Oxford Street, 012 United States
+                </a>
+                <a class="footer-top__email" href="mailto:youremail@gmail.com">youremail@gmail.com</a>
+                <a class="footer-top__phone" href="tel:+02838388393">+0283 838 8393</a>
+                <ul class="footer-top__social-list">
+                    <li class="footer-top__social-item">
+                        <a class="footer-top__social-link" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="9" height="18">
+                                <path d="M6.19 3.38H9V0H6.19a3.94 3.94 0 0 0-3.94 3.94v1.69H0V9h2.25v9h3.38V9h2.81L9 5.63H5.63V3.94a.57.57 0 0 1 .56-.56Z"/>
+                            </svg>
+                        </a>
+                    </li>
+                    <li class="footer-top__social-item">
+                        <a class="footer-top__social-link" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="13">
+                                <path d="M16 1.54a6.47 6.47 0 0 1-1.88.52A3.3 3.3 0 0 0 15.56.24a6.6 6.6 0 0 1-2.08.8A3.28 3.28 0 0 0 7.8 3.37a3.24 3.24 0 0 0 .08.63A9.32 9.32 0 0 1 1.12.6 3.29 3.29 0 0 0 2.13 5a3.28 3.28 0 0 1-1.48-.43 3.28 3.28 0 0 0 2.63 3.26 3.29 3.29 0 0 1-.87.12 3.37 3.37 0 0 1-.62-.06 3.29 3.29 0 0 0 3.06 2.28 6.58 6.58 0 0 1-4.07 1.4 6.48 6.48 0 0 1-.78 0A9.28 9.28 0 0 0 5 13a9.28 9.28 0 0 0 9.34-9.34v-.42A6.61 6.61 0 0 0 16 1.54Z"/>
+                            </svg>
+                        </a>
+                    </li>
+                    <li class="footer-top__social-item">
+                        <a class="footer-top__social-link" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="16">
+                                <path d="M7 5h3v2h.05a4 4 0 0 1 3.51-1.68c3.75 0 4.44 2.16 4.44 5V16h-4v-5.26c0-1.25 0-2.87-1.86-2.87S10 9.24 10 10.65V16H7ZM0 5h4v11H0ZM4.08 1.85A1.85 1.85 0 1 1 2.23 0a1.85 1.85 0 0 1 1.85 1.85Z"/>
+                            </svg>
+                        </a>
+                    </li>
+                    <li class="footer-top__social-item">
+                        <a class="footer-top__social-link" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="17.17" height="17.52">
+                                <path d="M8.76 7.51v3h5a4.73 4.73 0 0 1-5 3.78 5.53 5.53 0 0 1 0-11.06 4.92 4.92 0 0 1 3.49 1.35l2.38-2.29A8.48 8.48 0 0 0 7.94 0 8.55 8.55 0 0 0 .52 5.69a8.79 8.79 0 0 0 8.24 11.83c5.05 0 8.41-3.55 8.41-8.56A8.07 8.07 0 0 0 17 7.51Z"/>
+                            </svg>
+                        </a>
+                    </li>
+                    <li class="footer-top__social-item">
+                        <a class="footer-top__social-link" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="12.45" height="16">
+                                <path d="M5.12 10.58c-.42 2.2-.94 4.32-2.46 5.42-.47-3.33.69-5.83 1.23-8.48C3 6 4 2.87 5.93 3.63c2.38.94-2.06 5.74.92 6.34s4.38-5.4 2.45-7.36c-2.79-2.83-8.12-.07-7.46 4C2 7.59 3 7.89 2.25 9.25.47 8.86-.06 7.45 0 5.57A6 6 0 0 1 5.42.05c3.36-.38 6.52 1.23 7 4.4.49 3.57-1.52 7.43-5.11 7.16a4.35 4.35 0 0 1-2.19-1.03Z"/>
+                            </svg>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer-top__item footer-top__item-nav">
+                <h4 class="footer-top__title">Useful Links</h4>
+                <ul class="footer-top__list">
+                    <li class="footer-top__item">
+                        <a class="footer-top__link" href="#">About Us</a>
+                    </li>
+                    <li class="footer-top__item">
+                        <a class="footer-top__link" href="#">Privacy Policy</a>
+                    </li>
+                    <li class="footer-top__item">
+                        <a class="footer-top__link" href="#">Terms & Conditions</a>
+                    </li>
+                    <li class="footer-top__item">
+                        <a class="footer-top__link" href="#">Contact Us</a>
+                    </li>
+                    <li class="footer-top__item">
+                        <a class="footer-top__link" href="#">Help & Support</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer-top__item footer-top__item-nav">
+                <h4 class="footer-top__title">My Account</h4>
+                <ul class="footer-top__list">
+                    <li class="footer-top__item">
+                        <a class="footer-top__link" href="#">My Account</a>
+                    </li>
+                    <li class="footer-top__item">
+                        <a class="footer-top__link" href="#">My Cart</a>
+                    </li>
+                    <li class="footer-top__item">
+                        <a class="footer-top__link" href="#">My WIshlist</a>
+                    </li>
+                    <li class="footer-top__item">
+                        <a class="footer-top__link" href="#">Registration</a>
+                    </li>
+                    <li class="footer-top__item">
+                        <a class="footer-top__link" href="#">Check Out</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer-top__item footer-top__item-form">
+                <h4 class="footer-top__title">Subscribe Our Newsletter</h4>
+                <p class="footer-top__text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.
+                </p>
+                <form class="footer-top__form" action="#">
+                    <input class="footer-top__form-input" type="email" placeholder="Your email address" required>
+                    <button class="footer-top__form-btn" type="submit">Subscribe</button>
+                </form>
+            </div>
+            
+        </div>
+        <div class="footer-bottom">
+            <p class="footer-bottom__copy">
+                © 2019 CodeAstrology. All Rights Reserved.
+            </p>
+            <div class="footer-bottom__accept">
+                We Accept
+                <img class="footer-bottom__accept-img" src="<?php echo get_template_directory_uri() ?>/app/images/icons/master-card.svg" alt="We accept Master Card">
+                <img class="footer-bottom__accept-img" src="<?php echo get_template_directory_uri() ?>/app/images/icons/visa.svg" alt="We accept Visa">
+                <img class="footer-bottom__accept-img" src="<?php echo get_template_directory_uri() ?>/app/images/icons/pay-pal.svg" alt="We accept PayPal">
+            </div>
+        </div>
+    </div>
+</footer>
+
+
 <?php wp_footer(); ?>
 
 </body>
--- wp-content/themes/marcho/page.php
+++ wp-content/themes/marcho/page.php
@@ -1,12 +1,14 @@
 <?php get_header(); ?>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php 
-	
-?>
-<?php endwhile; else: ?>
-	<p><?php echo esc_html__('Content not found.', 'wawe_setup'); ?></p>
-<?php endif; ?>
 
+<main class="main">
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	<?php 
+		
+	?>
+	<?php endwhile; else: ?>
+		<p><?php echo esc_html__('Content not found.', 'wawe_setup'); ?></p>
+	<?php endif; ?>
+</main>
 
 
 <?php get_footer();
