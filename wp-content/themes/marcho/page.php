3a61b9b3e7c2450fb1f5e2cd324520ff8d50a02e
Виталий Изотов <allmarket198@gmail.com>
2023-03-11 19:42:02 +0200

    Add section Ctegories Info for Marcho

--- wp-content/themes/marcho/page.php
+++ wp-content/themes/marcho/page.php
@@ -4,6 +4,7 @@
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<?php 
 		get_template_part('template-parts/content','top-slider');
+		get_template_part('template-parts/content','categories-info');
 	?>
 	<?php endwhile; else: ?>
 		<p><?php echo esc_html__('Content not found.', 'wawe_setup'); ?></p>
