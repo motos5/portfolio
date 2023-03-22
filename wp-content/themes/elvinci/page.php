12bc720501d4a0c0080ec9e219c601c521d5d297
Виталий Изотов <allmarket198@gmail.com>
2023-03-22 19:49:43 +0200

    Add Blog for Elvinci

--- wp-content/themes/elvinci/page.php
+++ wp-content/themes/elvinci/page.php
@@ -3,6 +3,8 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php 
 	get_template_part('template-parts/content','top-slider');
+	get_template_part('template-parts/content','blog');
+
 ?>
 <?php endwhile; else: ?>
 	<p><?php echo esc_html__('Content not found.', 'wawe_setup'); ?></p>
