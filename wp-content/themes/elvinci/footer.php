7248982491917758cc4da418425dd9c5a0317cce
Виталий Изотов <allmarket198@gmail.com>
2023-03-22 21:34:09 +0200

    Add Footer for Elvinci

--- wp-content/themes/elvinci/footer.php
+++ wp-content/themes/elvinci/footer.php
@@ -1,3 +1,53 @@
+<footer class="footer">
+    <div class="container">
+        <div class="footer__inner">
+            <div class="footer__text">
+                <p>
+                    Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla mattis purus eget gravida aliquet. Suspendisse potenti. Integer sit amet blandit ante.  Suspendisse potenti. Integer...
+                </p>
+            </div>
+            <ul class="footer__category-list">
+                <li class="footer__category-item">
+                    <a class="footer__category-link" href="#">Kategorie-1</a>
+                </li>
+                <li class="footer__category-item">
+                    <a class="footer__category-link" href="#">Kategorie-2</a>
+                </li>
+            </ul>
+            <ul class="footer__tag-list">
+                <li class="footer__tag-item">
+                    <a class="footer__tag-link" href="#">#LongtextTag-1</a>
+                </li>
+                <li class="footer__tag-item">
+                    <a class="footer__tag-link" href="#">#Tag-2</a>
+                </li>
+                <li class="footer__tag-item">
+                    <a class="footer__tag-link" href="#">#Tag-5</a>
+                </li>
+            </ul>
+            <div class="footer__social">
+                <h4 class="footer__social-title">Follow us:</h4>
+                <ul class="footer__social-list">
+                    <li class="footer__social-item">
+                        <a class="footer__social-link" href="#">
+                            <img src="<?php echo get_template_directory_uri() ?>/assets/img/socials/twitter.svg" alt="">
+                        </a>
+                    </li>
+                    <li class="footer__social-item">
+                        <a class="footer__social-link" href="#">
+                            <img src="<?php echo get_template_directory_uri() ?>/assets/img/socials/instagram.svg" alt="">
+                        </a>
+                    </li>
+                    <li class="footer__social-item">
+                        <a class="footer__social-link" href="#">
+                            <img src="<?php echo get_template_directory_uri() ?>/assets/img/socials/facebook.svg" alt="">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
 <?php wp_footer(); ?>
 
 </body>
