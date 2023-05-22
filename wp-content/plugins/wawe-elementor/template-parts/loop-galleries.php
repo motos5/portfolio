48fab218dc82707cd187c2aa0bc9643e543bd26e
Виталий Изотов <allmarket198@gmail.com>
2023-05-22 19:14:54 +0300

    delete

--- wp-content/plugins/wawe-elementor/template-parts/loop-galleries.php
+++
@@ -1,4 +0,0 @@
-<div class="gallery__content-item category-<?php // echo $button->slug; ?>">
-    <?php the_post_thumbnail( 'full'); ?>
-    <a class="gallery__item-link" href="#"></a>
-</div><!-- End cases content one-->
