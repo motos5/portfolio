810574449a3c08c1bf8f59adcf10fb699f8ed173
Виталий Изотов <allmarket198@gmail.com>
2023-05-27 16:06:49 +0300

    Delete Themes

--- wp-content/themes/wayup/searchform.php
+++
@@ -1,6 +0,0 @@
-<form class="searchform" role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-
-    <input class="text-search" type="search" name="s" value="<?php the_search_query(); ?>" placeholder="<?php echo esc_attr__('Search', 'wayup'); ?>">
-    <input type="submit" class="submit-search" value="" />
-
-</form>
